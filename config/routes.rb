497dd9d3446c4f44d06524b6b8ce0ee97b81609e
奈須拓実 <nasutaku7nasuka@docomo.ne.jp>
2019-12-15 14:12:47 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'products/index'
   post 'products/pay'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "products#index"
+  root to: "posts#index"
 end
